4b4339d54e57eff6a7a4ec58fa205faf8b4643e0
tester119 <daniil.andropov2016@gmail.com>
2019-12-31 04:43:02 +0300

    4ур ланшафт, деревья

---
+++ Assets/move_platform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class move_platform : MonoBehaviour
+{
+    //[SerializeField]
+    //private float speed = 1.0F;
+
+    //private Vector3 direction;
+
+    //private Transform[] head;
+
+    //private void Movie()
+    //{
+    //    transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+    //}
+    
+    //void Start()
+    //{
+    //    direction = transform.right;
+    //}
+    
+    //void Update()
+    //{
+    //    Movie();
+    //}
+
+    //protected virtual void OnTriggerEnter2D(Collider2D collider)
+    //{
+    //    NewHero unit = collider.GetComponent<NewHero>();
+
+    //    if (unit && unit is NewHero)
+    //    {
+    //        unit.transform.position = transform.position;
+    //    }
+    //}
+}
